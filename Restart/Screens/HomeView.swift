2828f26bd561435aecd4859467c7b35421688f39
Eduardo <eduardodiasdini@gmail.com>
2023-01-04 10:56:58 -0300

    Home screen + reusable view

--- Restart/Screens/HomeView.swift
+++ Restart/Screens/HomeView.swift
@@ -13,15 +13,45 @@ struct HomeView: View {
 
     // MARK: - BODY
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Home")
-                .font(.largeTitle)
+        VStack{
+            // MARK: - HEADER
+
+            Spacer()
+
+            ZStack {
+                CircleGroupView(shapeColor: .gray, shapeOpacity: 0.1)
+
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                .padding()
+            }
+
+            // MARK: - BODY
+
+            Text("The time that leads to mastery is dependent on the intensity of our focus")
+                .font(.system(.title3, weight: .light))
+                .foregroundColor(.secondary)
+                .multilineTextAlignment(.center)
+                .padding()
+
+            // MARK: - FOOTER
+
+            Spacer()
 
             Button(action: {
                 isOnboardingViewActive = true
             }) {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
                 Text("Restart")
+                    .font(.system(.title3, design: .rounded, weight: .bold))
             }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large
+        )
+
         }
     }
 }
