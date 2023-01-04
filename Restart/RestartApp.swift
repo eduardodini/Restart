e2a8bad9968bd73797a6d944de4b6cb902954262
Eduardo <ediasdini@apple.com>
2023-01-04 09:54:21 -0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Eduardo Dini on 04/01/23.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
